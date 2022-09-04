a15a491331833a92f8306665fb0e418364219e58
Victor Asir <112397517+victorasir1208@users.noreply.github.com>
2022-09-05 00:41:17 +0530

    Update server_start.sh

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo chmod +x /home/ec2-user/apache-tomcat-9.0.65/bin/./startup.sh
-sudo /home/ec2-user/apache-tomcat-9.0.65/bin/./startup.sh
+sudo chmod +x /opt/tomcat/bin/./startup.sh
+sudo /opt/tomcat/bin/./startup.sh
